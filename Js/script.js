4c2cd292f2162c355000db3855ebcc2f5e8fbac5
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2023-09-18 15:22:15 +0500

    float

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+let numbers;
+
+function fibnachi() {
+    for (i =1 ; i % 50 ==0; i++){
+        return (numbers)
+    }
+}// a attemp for fibnachi series to develop a function that will return fibnachi series
+
