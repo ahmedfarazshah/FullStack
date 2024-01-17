9acce49bd097fd71f731299038c1624aa295d643
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2024-01-17 12:14:43 +0500

    javascript basics

---
+++ Js/Excercise_1/script.js
@@ -0,0 +1,25 @@
+function test() {
+    var a = '3';
+    var b = '8';
+
+
+        /* write a code 7 to 8 lines of code here that will make "a " equal to 8 and "b" equal to 3
+         restirctions:
+         1. You are not allowed to change any variable.
+         2. You are not allowed to add any numbers.
+         3. You should not declare any varibles.
+         
+         */
+
+    // write your code here
+
+        var c = a ;
+        a = b;
+        b = c;
+
+    console.log("a is equal to" + a);
+    console.log("b is equal to" + b);
+
+} 
+
+// Document.getElementsByClassName('click').onClick(test());
