60733a8f41377563168b33cdc34c0f0ed48e43a3
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2024-01-17 17:39:10 +0500

    slice

--- Js/Excercise_1/charactersLength.js
+++ Js/Excercise_1/charactersLength.js
@@ -12,3 +12,11 @@ var wordsLeft = totalWords - wordswritten;
 var wordText = ("Total number of characters: " + totalWords + " 2. characters written :" + wordswritten + " 3. characters Remaining: " + wordsLeft);
 
 alert(wordText);
+
+//  this is for the twitter character counter
+
+var essay = prompt("Write message, note that max limit is 180 characters");
+console.log(essay);
+
+var final = essay.slice(0, 180);
+alert(final);
