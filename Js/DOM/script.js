53ead4a6628439152de9e8a7bdcb2c5e57dfec22
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2024-02-03 10:18:13 +0500

    messing up with the selectors

--- Js/DOM/script.js
+++ Js/DOM/script.js
@@ -1,2 +1,18 @@
-// document.querySelector("li").innerHTML= "is it working";  // it will also removes the anchor tag
+// Know that when there is elements its an array 
+// and when it is only element it returns the value of the selected item
 
+// do remember that the selectors are always inside the qoutation marks due to the similarity in the inline js
+
+
+// there are several types of selectors 
+
+// document.firstElemetchild.lastELementChild.nextElementSibiling // returns only the elemet
+// document.getElemetsByClass("") // returns an array
+// document.getElementByClass("selector") //  returns the element as the id is specific
+// document.querySelector(".file, #item")           // the selection of selector's working is similar to the selection of selectors in css  
+// document.querySelector("li").innerHTML= "is it working";  // it will also removes the anchor tag as you have asked to do the specific task;
+
+
+
+
+//document.firstElementChild.lastElementChild.lastElementChild.previousElementSibling.previousElementSibling.lastElementChild.innerHTML = "hey he is always messing with me";
