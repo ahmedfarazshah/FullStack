ef4999c97ba2951be67c3246ef8ce9e825588f36
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2024-02-14 14:22:19 +0500

    jsut shufling

--- Js/CapturingxBubling/script._divCreation.js
+++ Js/CapturingxBubling/script._divCreation.js
@@ -5,3 +5,10 @@ newDiv.style.width = "200px";
 newDiv.style.height = "200px";
 newDiv.style.backgroundColor = "red"
 document.body.append(newDiv)
+
+
+newDiv.addEventListener("click", function(){
+    alert("hey I have got clicked")
+    document.body.style.backgroundColor = "red";
+} 
+)
--- Js/CapturingxBubling/script.js
+++
@@ -1,5 +0,0 @@
-var newDiv = document.createElement("div")
-newDiv.style.width = "200px";
-newDiv.style.height = "200px";
-newDiv.style.backgroundColor = "red"
-document.body.append(newDiv)
