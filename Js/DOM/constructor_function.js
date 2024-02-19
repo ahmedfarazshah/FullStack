fa2a0dadc7c206b79f03f3b85bbef1e7fef38202
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2024-02-19 10:28:00 +0500

    somehow drumkit is done

--- Js/DOM/constructor_function.js
+++ Js/DOM/constructor_function.js
@@ -17,3 +17,20 @@ function Person(name, resident, age, married){
 var person1 = new Person("mena", "Gujrat", 22, false)
 
 // thus a object is created using constructor function
+
+
+function Housekeeper(name, age , yearsOfExperience , cleaningRepertoire){
+    this.name = name;
+    this.age = age;
+    this.yearsOfExperience = yearsOfExperience;
+    this.cleaningRepertoire = cleaningRepertoire;
+    this.suitcaseShift = function(){
+        alert("Hi I'm " + this.name + " Can I help you with your suitcase")
+        // pickupSuitcase();
+        // move();
+    }
+    this.clean = function(){
+        alert("Okay it will be cleaned with in couples of minutes")
+    }
+    
+}
--- Websites/DrumKit/script.js
+++ Websites/DrumKit/script.js
@@ -36,7 +36,7 @@ setTimeout(() => {
 var numOfDrums = document.querySelectorAll(".drum").length
 
 for(var i = 0; i < numOfDrums; i++){
-    document.querySelectorAll(".drum")[i].addEventListener('click', function() {
+    document.querySelectorAll(".drum")[i].addEventListener('click', function(e) {
         var selec = this.textContent;
         switch (selec) {
             case 'w':
@@ -47,6 +47,7 @@ for(var i = 0; i < numOfDrums; i++){
 
             case 'a':
                 var tom2 = new Audio("sounds/tom-2.mp3" )
+
                 tom2.play();
             
                 break;
@@ -77,8 +78,53 @@ for(var i = 0; i < numOfDrums; i++){
              default:
                 alert("something fishy fishy")
         }
+
+
+    
         // this.style.color= 'white' // this keyword selects the current item
         // e.target.style.color = 'blue' // can also do using arrow functions
+    })
+
+}
+var num = document.querySelectorAll(".drum").length
+for (var i = 0; i < num ; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("keypress", function(e){
+        switch(e.key){
+            case 'w':
+                var tom1 = new Audio('sounds/tom-1.mp3')
+                tom1.play()
+                break;
+            case ("a"):
+                var tom2 = new Audio('sounds/tom-2.mp3')
+                tom2.play();
+                break;
+            case("s"):
+                var tom3 = new Audio("sounds/tom-3.mp3")
+                tom3.play();
+                break;
+            case ('d'):
+                var tom4 = new Audio("sounds/tom-4.mp3")
+                tom4.play();
+                break;
+            case 'j':
+                    var crash = new Audio('sounds/crash.mp3')
+                    crash.play();
+                    break;
+            case "k":
+                    var kickBass = new Audio("sounds/kick-bass.mp3")
+                    kickBass.play();
+                    break;
+    
+            case 'l':
+                    var snare = new Audio('sounds/snare.mp3')
+                    snare.play()
+                    break;
+    
+            default:
+        
+        }
+
     })
 }
 
@@ -129,9 +175,17 @@ instead of switch if we have used if else statements then this was the code
 // document.addEventListener('keypress', function(e){
 //     console.log(e)
 // })
-document.addEventListener('click', (e) => {
-    console.log(e)
-}) // log the event that is happening
+// document.addEventListener('click', (e) => {
+//     console.log(e)
+// }) // log the event that is happening
 
 
 
+// document.addEventListener("keypress", function(e) {
+//     console.log(e)
+//     // switch (e.key){
+//     //     case "d":
+//     //         alert("D is presssed")
+//     // }
+
+// })
