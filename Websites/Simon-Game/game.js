18dd97bcc0a9d8831cf4a54a5224072fcdb0725a
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2024-02-25 12:14:58 +0500

    now somethings working

--- Websites/Simon-Game/game.js
+++ Websites/Simon-Game/game.js
@@ -1,1 +1,41 @@
-alert("hello");
+
+var gamePattern = []
+
+
+var userClickedPatterns = []
+
+
+var buttonColors = ["red", "blue", "green", "yellow"]
+
+
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4)
+    var randomChoosenColor = buttonColors[randomNumber]
+    gamePattern.push(randomChoosenColor)
+
+    $("#"+randomChoosenColor).fadeOut(100).fadeIn(100)
+
+    sound(randomChoosenColor)
+
+}
+
+$(".btn").click(function(){
+    var userChoosenColor= this.id;
+    userClickedPatterns.push(userChoosenColor);
+    sound(userChoosenColor)
+    animateOnPress(this.id)
+})
+
+function sound(name){
+    var soundToPlay = new Audio("Assets/sounds/"+name+".mp3")
+    soundToPlay.play()
+
+}
+
+function animateOnPress(currentColor){
+    $("#"+currentColor).addClass("pressed")
+    setTimeout(() => {
+        $("#"+currentColor).removeClass('pressed')
+    }, 80);
+}
