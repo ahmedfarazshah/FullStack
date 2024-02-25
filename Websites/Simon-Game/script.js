75ac6c18aaf3a9d873c725a3679044b7f698e28b
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2024-02-25 08:56:32 +0500

    from the app brewery

---
+++ Websites/Simon-Game/script.js
@@ -0,0 +1,17 @@
+// var randomNumber, buttonColors, randomChoosenColor, gamePattern;
+
+// randomNumber = Math.floor(Math.random()*4);
+// buttonColors = ["red", "blue", "green", "yellow"]
+// randomChoosenColor = buttonColors[randomNumber]
+
+// $('#'+randomNumber).click(function(){
+//     var playFile = new Audio("Assets/sounds/"+randomChoosenColor+".mp3")
+//     playFile.play()
+    
+//     $('#'+randomNumber).fadeOut(10).fadeIn(10);
+
+
+// })
+
+
+// use the game script 
