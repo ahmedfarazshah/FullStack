ead190bc6805422821e4b0f2211cb5e1e3e8bbf5
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2024-03-03 13:32:14 +0500

    npm is love

---
+++ Back-end/Node/NPM/index.js
@@ -0,0 +1,18 @@
+var sillyName = require("sillyname")
+var hash = require("hash.js")
+const filesystem = require("fs")
+
+var namesx = sillyName();
+var file = `hey my name is ${namesx}`
+var hashedfile = hash.sha256().update(`${file}`).digest("hex")
+
+// THE NEW NAME AND NEW HASH VALUE WILL BE STORED
+// filesystem.writeFile("hashed_message.txt", `the hash of file is : ${hashedfile},\n the file is :${file}`, err => {
+//     if(err)throw err;
+//     console.log("the hash value has been stored")
+// })
+
+filesystem.readFile("./hashed_message.txt", "utf-8", (err, data)=>{
+    if(err)throw err;
+    console.log(data)
+})
---
+++ Back-end/Node/Native_Modules/index.js
@@ -0,0 +1,17 @@
+// we will use the file system module
+// & will create a file in the directory of our own will 
+
+const { error } = require("console")
+const fileSystem = require("fs")
+
+// creating a file of txt extension 
+// fileSystem.writeFile("message.txt", "Don't tell anyone about it", (error) => {
+//     if(error) {throw error};
+//     console.log("The message has been saved")
+// })
+
+// fetching the file
+fileSystem.readFile("./message.txt","utf-8", (err, fileData)=> {
+    if(err) throw err;
+    console.log(fileData)
+})
