0e90668050d40a4ad608cb658795298b0665f990
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2024-03-09 12:50:34 +0500

    first application

---
+++ Back-end/ExpressJS/Calculator/calculator.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express();
+
+
+app.get("/", (req, res)=>{
+    res.send("<button> first Number</button>")
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("the server is tuned to 3000 port")
+})
