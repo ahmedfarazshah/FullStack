3795c147a928386d7e21df641c2b997b7285d425
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2024-03-14 16:03:16 +0500

    working on apis

---
+++ Back-end/API/experimental/app.js
@@ -0,0 +1,72 @@
+const express = require("express")
+const app = express()
+const https = require("https")
+const bodyParser = require("body-parser")
+const { join } = require("path")
+app.use(bodyParser.urlencoded({extended:true}))
+// const bodyParser = require("body-parser")// incase if you used a form to get data from the client
+// app.use(bodyParser({extenable: true}))
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/index.html")
+    // res.send('nothing') will th
+    // res.send()//it will either send file or run the send which is write (response)
+})
+
+app.post("/", (req,res)=>{
+   
+    //for the kanye qoutes
+    const url = "https://api.kanye.rest"
+    https.get(url, (response)=>{
+        response.on('data', data =>{ // the string and the parameter should be the same to fetch the data
+            const kanyeSays = JSON.parse(data)
+            // res.write(kanyeSays.qoute)
+            res.write(kanyeSays.quote)
+            res.send()
+        })
+    })
+
+
+})
+
+
+app.post("/countries" , (req, res)=>{
+    const userInputCountry = req.body.country;
+
+    url = "https://restcountries.com/v3.1/name/"+userInputCountry
+    https.get(url, response =>{
+        response.on("data", data=>{
+            
+            const countryInfo = JSON.parse(data)
+            const icon = JSON.stringify( countryInfo[0].flags.png)
+            console
+            
+            res.write("Country : " + countryInfo[0].name.common +"\n")
+            res.write("Offical Name of Country : "+ countryInfo[0].name.official +"\n") 
+            res.write("Capital City : " + countryInfo[0].capital +"\n")
+            res.write("Continent : " + countryInfo[0].region + " sub : " + countryInfo[0].subregion +"\n")
+            res.write("Borders with : " + countryInfo[0].borders +"\n")
+            res.write("Area : " + countryInfo[0].area +"\n")
+            res.write("Population : " + countryInfo[0].population +"\n")
+            res.write(`<img src= ${(icon)}></img> \n`)
+            res.send()
+
+        })
+    })
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+app.listen(1122, function(){
+    console.log("The server is running at port 1122")
+})
