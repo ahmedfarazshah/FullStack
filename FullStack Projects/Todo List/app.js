e49c75db096e45fc347bb530d64f6029613f5016
Ahmed Faraz Shah <ahmedfarazshah42@gmail.com>
2024-03-24 11:56:24 +0500

    files have been added

--- FullStack Projects/Todo List/app.js
+++ FullStack Projects/Todo List/app.js
@@ -1,45 +1,34 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+var express = require("express")
+var bodyParser = require("body-parser")
 
 const app = express()
 
+app.use(express.static("public"))
+app.set("view engine" , "ejs")
+app.use(bodyParser.urlencoded({extended: true}))
+
+var items = ["Buy Books", "Read Books", "Code"]
 app.get("/", (req, res)=>{
     const today = new Date() // created the date object so that we will be able to fetch the current day and date
-    const currentDay = today.getDay()
 
-    if(currentDay === 6 || currentDay === 0){
-        res.send("Hurray It is weekend and I can take a day or two off")
-    }else{
-        res.sendFile(__dirname + "/index.html")
-    }
+    var options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long",
+        year : "numeric",
+    }    
+    var day = today.toLocaleDateString("ur-Ur", options) // uses the string format as setted in the options
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    res.send("hi")
+        // res.send("Hurray It is : " + day + " and is a weekend") // uncommenting so that we are using ejs file
+        res.render("list", {WhichDay: day, newlyListed: items})
+    
 })
 
-
-
-
-
-
-
-
-
-
+app.post("/", (req,res)=>{
+    var newInput = req.body.whatTodo
+    items.push(newInput)
+    res.redirect("/")
+})
 
 
 
