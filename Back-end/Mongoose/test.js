9762e1f02f53722b2f2a4c18e3696f5408de4084
ahmedfarazshah <ahmedfarazshah42@gmail.com>
2024-04-23 16:24:36 +0500

    quering in mongoose

---
+++ Back-end/Mongoose/test.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/Real_State")
+
+const houseSchema = new mongoose.Schema({
+    Location : String,
+    land_in_acre : Number,
+    number_of_rooms : Number
+})
+const landSchema = new mongoose.Schema({
+    location : String,
+    land_in_acre : Number
+})
+
+
+// making collection
+
+const homes = mongoose.model("home", houseSchema)
+const land = mongoose.model("land", landSchema)
+
+// multiple of homes 
+
+const house1 = new homes ({
+    location : '1234london',
+    land_in_acre : 33,
+    number_of_rooms : 5
+})
+const house2 = new homes ({
+  location : "texas 132",
+  land_in_acre : 33,
+  number_of_rooms : 2
+})
+
+// homes.insertMany([house1, house2])
+
+const land_danyore = new land ({
+    location: "amphari danyore",
+    land_in_acre : 30
+})
+// land_danyore.save().then(function(){console.log("danyore land listed")})
+
+
+// console.log(homes.find({}).toArray())
+// land.find({})
+// Find all lands
+async function findLands() {
+    try {
+      const lands = await land.find();
+      console.log("Found lands:", lands);
+    } catch (error) {
+      console.error("Error finding lands:", error);
+    }
+  }
+findLands()  
