e8e3157e691b8f3179c07665eefb6bf3b1c94135
ahmedfarazshah <ahmedfarazshah42@gmail.com>
2024-04-25 13:02:54 +0500

    mongoose crud completed

--- Back-end/Mongoose/app.js
+++ Back-end/Mongoose/app.js
@@ -5,12 +5,12 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB") // this will connect and 
 // will make a schema so that we will use in model
 
 const fruitSchema = new mongoose.Schema({
-    name : {type : String, required : true, },
+    name : {type : String, required : true, unique: true},
     season : String,
     rating : {type: Number , min : 1, max :[10, 'standard type of rating 1-10']}
 })
 const peopleSchema = new mongoose.Schema({
-    id: {type : Number , unique : true, required: true},
+    id: {type : Number , required: true},
     name : String,
     age : {type :Number, min: 18, max:101}
 })
@@ -28,8 +28,8 @@ const mango = new summer({
     season : "Summer",
     rating: 9
 })
-const person2 = new people({
-    id: 1,
+const person = new people({
+    id: 3,
     name : "John Cena",
     age : 44
 })
@@ -49,6 +49,7 @@ const gindawar = new winter({
     rating : 5
 })
 
+// person.save().then(function(){console.log("person added")})
 // to save or add records to the collection we use .save
 // apple.save()
 
@@ -56,7 +57,61 @@ const gindawar = new winter({
 
 // to insert many we use the model : Fruits.insertMany([apple, orange, banna], function(err, data){})
 // winter.insertMany([orange, harmit, gindawar])
-// person2.save().then(()=>{console.log("You can see me")})
+person.save()
+
+
+
+
+//////////////////////////////////
+// Update - U
+
+// summer.updateOne({_id: "6628d965fe449ff0eafa715f"}, {name: "Peach", rating: 6})
+// summer.updateOne({_id: "6628d965fe449ff0eafa715f"}, {name: "Peach", rating:6})
+//     .then(() => {
+//             console.log("Document updated successfully!");
+//     })
+//     .catch(error => {
+//         console.error("Error updating document:", error);
+//     });
+
+// Update many
+// summer.updateMany({season:"Summer"}, {rating: 8})
+// .then(function(){
+//     console.log("values upadated")
+// }).catch(err =>{
+//     console.log("error :" , err)
+// })
+
+
+
+/////////////////////////////////////////
+/// Delete - D
+
+
+// person.deleteOne({_id : "66275dd948e9baaa7a9c9978"})
+// .then(()=>{
+//     console.log("Angela deleted Successfully")
+// }).catch(err=>{
+//     console.log("error :" , err)
+// })
+
+
+// people.deleteOne({age : 29}).then(()=>{
+//     console.log("deleted id 3 successful")
+// }).catch(err=>{
+//     console.log("error found :" , err)
+// })
+
+
+
+
+
+
+
+
+////////////////////////////////////
+///// Read - R
+/*
 
 async function winterFruits(criteria= {}, projection = {}){
     try{
@@ -64,7 +119,7 @@ async function winterFruits(criteria= {}, projection = {}){
     winfruits.forEach((data)=>{
         console.log(data.name , "rating : ", data.rating)
     })
-    mongoose.disconnect()
+    // mongoose.disconnect()
 
 }catch (err) {
     console.log("error found ",err)
@@ -84,20 +139,21 @@ async function findPeople(){
     }
     
 }
+
 async function summerFruits(){
     try{
         const sumfruits = await summer.find()
         sumfruits.forEach(data=>{
             console.log(data.name)
         })
-        mongoose.disconnect()
+        // mongoose.disconnect()
         console.log("connection closed")
     }catch (err){
         console.log("connection closed err", err)
     }
     
 }   
-
+*/
 // winterFruits()
 // findPeople()
 // summerFruits()
