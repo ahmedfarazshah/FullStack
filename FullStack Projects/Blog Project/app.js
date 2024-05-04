2ff6651bf09ac936554d980ba2c761f93b3943dc
ahmedfarazshah <ahmedfarazshah42@gmail.com>
2024-05-04 12:02:17 +0500

    blog project in datavase

--- FullStack Projects/Blog Project/app.js
+++ FullStack Projects/Blog Project/app.js
@@ -1,23 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash")
+const _ = require("lodash");
+const mongoose = require("mongoose")
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/BlogProject")
+
+const BlogSchema = mongoose.Schema({
+  title : String,
+  content : {type : String}
+})
+
+const blogs = mongoose.model("blog", BlogSchema)
+
+// created the database and blogs collection
+
+// const posts = []
+
+// blogs.find({}).then(data=>{console.log(data)})
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = []
-
 app.get("/", (req, res)=>{
-  res.render("home", {dummyContent: homeStartingContent, post : posts})
+
+
+
+  res.render("home", {dummyContent: homeStartingContent, post : blogs})
 })
 
 app.get("/about",(req,res)=>{
@@ -34,11 +50,15 @@ app.get("/compose", (req, res)=>{
 
 
 app.post("/compose", (req,res)=>{
-  const userPost = {
+
+  const post1 = new blogs({
     title : req.body.userContentTitle,
     content : req.body.userContentPost
-  }
-  posts.push(userPost)
+  })
+  post1.save()
+  
+  
+  
   res.redirect("/")
 })
 
