cd54abdd4fe9bdea2f765c94a9213bd2de5c4493
ahmedfarazshah <ahmedfarazshah42@gmail.com>
2024-05-08 00:57:10 +0500

    security layer 2

--- Authentication Projects/Secrets/app.js
+++ Authentication Projects/Secrets/app.js
@@ -2,12 +2,23 @@
 // const bodyParser = require("body-parser") // no longer required as not it comes with the express newer versions 
 
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
 
 app.use(express.static("public"))
 app.use(express.urlencoded({extended : true}))
 app.set('view engine', 'ejs')
+
+//////////////////////////////////////////////// Database setup ///////////////////////////
+mongoose.connect("mongodb://localhost:27017/userDB")
+
+const userSchema = mongoose.Schema({
+    email : String,
+    password : String
+})
+
+const Users = mongoose.model("user", userSchema)
 
 
 //////////////////////////////////////////////////// Get Verbs(REST) /////////////////////////////////////////
@@ -24,17 +35,29 @@ app.get("/register", (req, res)=>{
     res.render("register")
 })
 
-app.get("/secrets", (req, res)=>{
-    res.render('secrets')
-})
-
-app.get("/submit", (req, res)=>{
-    res.render("submit")
-})
-
 ///////////////////////////////////////////////////// Post Verbs (REST) ///////////////////////////////////////
 
+app.post("/register", (req, res)=>{
+    const username = req.body.username
+    const pass = req.body.password
 
+    const newUser = new Users({
+        email : username, 
+        password : pass
+    })
+    newUser.save().then(()=>{res.render("secrets")}).catch(err=>{res.send(err)})
+})
+
+app.post("/login",(req, res)=>{ 
+    const username = req.body.username
+    const pass = req.body.password
+
+    Users.findOne({email : username}).then(data =>{
+        if(data.password === pass){
+            res.render("secrets")
+        }
+    }).catch(err=>{res.send(err)})
+})
 
 
 
