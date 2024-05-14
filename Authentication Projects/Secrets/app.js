b7a4ef6f0855892515290a8188be770bda78ce12
ahmedfarazshah <ahmedfarazshah42@gmail.com>
2024-05-14 15:26:01 +0500

    google auth somehow working

--- Authentication Projects/Secrets/app.js
+++ Authentication Projects/Secrets/app.js
@@ -6,7 +6,9 @@ const mongoose = require("mongoose")
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
 const session = require("express-session")
-const { resolveInclude } = require("ejs")
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
+
 
 
 const app = express()
@@ -15,12 +17,12 @@ app.use(express.static("public"))
 app.use(express.urlencoded({extended : true}))
 app.set('view engine', 'ejs')
 
-// app.set('trust proxy', 1) // trust first proxy
+app.set('trust proxy', 1) // trust first proxy
 app.use(session({
   secret: 'our little secret. ',
   resave: false,
-  saveUninitialized:false
-//   cookie: { secure: true } ///////////// this is for https secure
+  saveUninitialized:false,
+  cookie: { secure: true } ///////////// this is for https secure
 }))
 
 app.use(passport.initialize())
@@ -33,16 +35,59 @@ mongoose.connect("mongodb://localhost:27017/userDB")
 
 const userSchema = new mongoose.Schema({
     email : String,
-    password : String
+    password : String,
+    googleId: String // this will not create one if its is already there
 })
 userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 
 const Users = mongoose.model("user", userSchema)
 
 passport.use(Users.createStrategy());
 
-passport.serializeUser(Users.serializeUser()); // these both are only necessary when using sesions as it creates cookie with userid 
-passport.deserializeUser(Users.deserializeUser());
+// passport.serializeUser(Users.serializeUser()); // these both are only necessary when using sesions as it creates cookie with userid 
+// passport.deserializeUser(Users.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+
+ // USING GOOGLE OAUTH
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "https://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+    });
+  }
+));
 
 
 //////////////////////////////////////////////////// Get Verbs(REST) /////////////////////////////////////////
@@ -50,6 +95,17 @@ passport.deserializeUser(Users.deserializeUser());
 app.get("/", (req, res)=>{
     res.render("home")
 })
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets', 
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/secrets');
+    });
+
 
 app.get("/login", (req, res)=>{
     res.render("login")
@@ -73,6 +129,7 @@ app.get("/logout", (req, res)=>{
         res.redirect("/")
     })
 })
+
 
 ///////////////////////////////////////////////////// Post Verbs (REST) ///////////////////////////////////////
 
