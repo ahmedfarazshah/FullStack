1abdb49507715e24fa9036b76d78754078c1fcf3
ahmedfarazshah <ahmedfarazshah42@gmail.com>
2024-05-16 15:27:32 +0500

    deleted the old method

--- React/Project 1/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"; 
-import ReactDOM from "react-dom"
-import createRoot from "react-dom/client"
-
-// ReactDOM.render(<h1>hello World</h1>, document.getElementById("root")) this is depricated
-
-const root = createRoot(document.getElementById("root"))
-root.render(<h1>hello world</h1>) // this only takes one input but you can make child of one div 
