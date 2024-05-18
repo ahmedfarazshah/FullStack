e552ab4bad833c333bf7e6093cf56e85bf7454a5
ahmedfarazshah <ahmedfarazshah42@gmail.com>
2024-05-18 12:13:57 +0500

    import export done

--- React/Project 1/src/App.jsx
+++ React/Project 1/src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./header.jsx"
 import "./app.css" // this is another aproach for styling in react
 import Greet from "./greetings.jsx"
+import {add, subtract, multiply, divide} from "./calculator"
+
 
 function App(){
 
   const luckyNumbet = 3
   const date = new Date
+
   const imagesSize = { // for inline css in React
     width : "100px",
     height : "100px"
@@ -30,8 +33,20 @@ function App(){
         <img style={imagesSize} src={imagex + "?grayscale"} alt="Random image" />
         <img src="https://blog.getbootstrap.com/assets/brand/bootstrap-logo-shadow.png" alt="Bootstrap logo" />
         </div>
-      <h1>greet import</h1>
-      <Greet/>
+        <Greet />
+        <Greet />
+        <Greet />
+      <h1>Time In Hours : {date.getHours()}</h1>
+
+
+      <h2>Math </h2>
+      <ul>
+       <li>{add(2,3)}</li>
+       <li>{multiply(2,3)}</li>
+       <li>{subtract(2,3)}</li>
+       <li>{divide(2,3)}</li>
+      </ul>
+
     </>
   )
 }
--- React/Project 1/src/greetings.jsx
+++ React/Project 1/src/greetings.jsx
@@ -2,18 +2,35 @@
 // you can do moduleing if you intened to be specific
 // import styles from "./app.css"   /// if you dont use this, then a conflit will occur 
 // you cant use the same variable for 2 files or else your whole app will show an errrrrrrrrrorrrrrrrrrr but it wont display
+import coloring from "./coloring.module.css" // this is the naming convention for styling in react
+
 
 function Greet(){
-    const gm = "Good morning"
-    // const an = "Good Afternoon"
-    // const eve = "Good Evening"
 
+    const time = new Date()
+    const currenttime = time.getHours()
+    
+    const customstyle = {
+        color : ""
+    }
+
+    let greetings;
+    if(currenttime <= 12 ){
+        greetings = "Good Morning"
+        customstyle.color = "red"
+    }else if (currenttime < 18){
+        greetings = "Good Afternoon"
+        customstyle.color = "green"
+    }else{
+        greetings = "Good Evening"
+        customstyle.color = "blue"
+    }
 
 
     return(
         <>
         
-            <h1 className="header">{gm}</h1> 
+            <h1 className={coloring.hyper} style={customstyle}>{greetings +" : "+ currenttime} </h1> 
 
         
 
